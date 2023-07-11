b6e316468eef8a147c83c72f2f51e3e77aa3c908
syeminPark <soccer0874@gmail.com>
2023-07-11 17:35:29 +0900

    bye again

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 
 config={
-#'ChatOpenAI' 'huggingCustomEndpoint' 'wizardVicuna13_local' 'vicuna13b_replicate'  'falcon7b_octoAI'
+#'ChatOpenAI' 'huggingCustomEndpoint' 'wizardVicuna13_local' 'vicuna13b_replicate'  'falcon7b_octoAI' 'flan-t5-xxl'
 "LLM_Name":'vicuna13b_replicate'
 
 }
