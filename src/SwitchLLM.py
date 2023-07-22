189809bdc23c4bcc7ee842dbe30475325b164d68
syeminPark <soccer0874@gmail.com>
2023-07-22 15:09:19 +0900

    add an optio for the 70b model.

--- src/SwitchLLM.py
+++ src/SwitchLLM.py
@@ -29,6 +29,9 @@ def switchLLM():
     elif config['LLM_Name']=='llama_v2_13b_replicate':
         llm = Replicate(model="a16z-infra/llama13b-v2-chat:5c785d117c5bcdd1928d5a9acb1ffa6272d6cf13fcb722e90886a0196633f9d3",
                         input= {"max_length":8000,"max_new_tokens": 8000})
+    elif config['LLM_Name']=='llama_v2_70b_replicate':
+        llm = Replicate(model="replicate/llama70b-v2-chat:2d19859030ff705a87c746f7e96eea03aefb71f166725aee39692f1476566d48",
+                        input= {"max_length":8000,"max_new_tokens": 8000})
     elif config['LLM_Name']=='falcon7b_octoAI':
         llm = OctoAIEndpoint(
             model_kwargs={
--- src/config.py
+++ src/config.py
@@ -5,8 +5,12 @@ LLMList=['ChatOpenAI',
          'vicuna13b_replicate',
          'falcon7b_octoAI',
          'flan-t5-xxl',
-         'llama_v2_13b_replicate']
+         'llama_v2_13b_replicate',
+         'llama_v2_70b_replicate'
+         
+         
+         ]
 
 config={
-"LLM_Name": LLMList[6]
+"LLM_Name": LLMList[7]
 }
